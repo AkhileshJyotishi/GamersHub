113a9885adb9f7a0cca843507a8a2881273e7092
Yash7426 <yashagr74260@gmail.com>
2023-11-18 00:12:55 +0530

    Add Email functionality

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -14,17 +14,17 @@ router.post(
 router.post('/login', validate(authValidation.login), authController.login)
 router.post('/logout', validate(authValidation.logout), authController.logout)
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens)
-// router.post(
-//   '/forgot-password',
-//   validate(authValidation.forgotPassword),
-//   authController.forgotPassword
-// )
+router.post(
+  '/forgot-password',
+  validate(authValidation.forgotPassword),
+  authController.forgotPassword
+)
 router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword)
-// router.post(
-//   '/send-verification-email',
-//   validate(authValidation.verificationEmail),
-//   authController.sendVerificationEmail
-// )
+router.post(
+  '/send-verification-email',
+  validate(authValidation.verificationEmail),
+  authController.sendVerificationEmail
+)
 router.post('/verify-email', validate(authValidation.verifyEmail), authController.verifyEmail)
 router.post(
   '/add-provider/:userId',
