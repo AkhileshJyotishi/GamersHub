0d0fba32050b3ccede1692765f2b524a2a32ad5b
Yash7426 <lcs2022057@iiitl.ac.in>
2024-02-11 23:12:29 +0530

    Route fixes

--- src/routes/v1/admin.route.ts
+++ src/routes/v1/admin.route.ts
@@ -10,18 +10,6 @@ router.get('/blacklist', auth('getUsers'), adminController.getBlacklistedUser)
 router.get('/unblacklist', auth('getUsers'), adminController.getUnblacklistedUser)
 
 router.post(
-  '/blacklist/:userId',
-  auth('manageUsers'),
-  validate(adminValidation.paramsValidation),
-  adminController.blacklistUserById
-)
-router.post(
-  '/include/:userId',
-  auth('manageUsers'),
-  validate(adminValidation.paramsValidation),
-  adminController.unblacklistUserById
-)
-router.post(
   '/blacklist/email',
   auth('manageUsers'),
   validate(adminValidation.userEmailValidation),
@@ -32,6 +20,18 @@ router.post(
   auth('manageUsers'),
   validate(adminValidation.userEmailValidation),
   adminController.unblacklistUserByEmail
+)
+router.post(
+  '/blacklist/:userId',
+  auth('manageUsers'),
+  validate(adminValidation.paramsValidation),
+  adminController.blacklistUserById
+)
+router.post(
+  '/include/:userId',
+  auth('manageUsers'),
+  validate(adminValidation.paramsValidation),
+  adminController.unblacklistUserById
 )
 
 export default router
